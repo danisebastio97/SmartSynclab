7386a23db37a6c57e1d970a4cee2620afae5bab0
Daniele Sebastio <daniele.sebastio@synclab.net>
2022-07-29 11:25:52 +0200

    first commit

---
+++ android/app/src/main/kotlin/it/synclab/smart_synclab/smart_synclab/MainActivity.kt
@@ -0,0 +1,6 @@
+package it.synclab.smart_synclab.smart_synclab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:smart_synclab/login/login_page_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPageView(),
+    );
+  }
+}
